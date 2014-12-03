b10b0f459133b7813742c5d31dc178e4286cd553
Likeriver0 <Like_River@163.com>
2014-12-03 23:37:11 +0800

    zui lan de shi xian . gai dui le

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,7 +1,6 @@
 //TODO: Please write code in this file.
 
 
-
 function GetItemIndex(barcode){
     var index , i ;
     for(i = 0 ; i < loadAllItems().length ; i++){
@@ -50,39 +49,50 @@ function Settle_Account(inputs_new){
     //获取当前商品号对应的在列表中的位置
     var i , j = 0, zongyouhui = 0 ,zongjia = 0 ;
     var youhuiwuping = new Array();
+    var str;
 
-    console.log('***<没钱赚商店>购物清单***');
-
+//    console.log('***<没钱赚商店>购物清单***');
+    str = '***<没钱赚商店>购物清单***\n';
     for(i = 0 ; i < inputs_new.length ; i++){
         var index = GetItemIndex(inputs_new[i].split('-')[0]);
 
         //检测是否属于优惠商品
         if( WhetherPromotions( inputs_new[i].split('-')[0]) ) {
             //商品数量除以3就是优惠数量
-            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
-            zongyouhui = zongyouhui + Number(inputs_new[i].split('-')[1])/3 * loadAllItems()[index].price;
+//            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
+            str = str + '名称：'+loadAllItems()[index].name+'，'+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+'，'+'单价：'+(loadAllItems()[index].price).toFixed(2)+'(元)，'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)\n';
+            zongyouhui = zongyouhui + parseInt(Number(inputs_new[i].split('-')[1])/3) * loadAllItems()[index].price;
             zongjia = zongjia + ((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price);
             youhuiwuping[j] = loadAllItems()[index].name+'-'+parseInt(Number(inputs_new[i].split('-')[1])/3)+'-'+loadAllItems()[index].unit;
             j++
         }
         //不打折时
         else{
-            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
+//            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
+            str = str + '名称：'+loadAllItems()[index].name+'，'+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+'，'+'单价：'+(loadAllItems()[index].price).toFixed(2)+'(元)，'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)\n';
             zongjia = zongjia + ((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price);
         }
 
     }
 
-    console.log('----------------------')
-    console.log('挥泪送的商品:')
+//    console.log('----------------------')
+    str = str + '----------------------\n'
+//    console.log('挥泪赠送商品:')
+    str = str + '挥泪赠送商品：\n'
     for(i = 0 ; i < j ; i++ ){
-        console.log('名称：'+youhuiwuping[i].split('-')[0]+',数量：'+youhuiwuping[i].split('-')[1]+youhuiwuping[i].split('-')[2]);
+//        console.log('名称：'+youhuiwuping[i].split('-')[0]+',数量：'+youhuiwuping[i].split('-')[1]+youhuiwuping[i].split('-')[2]);
+        str = str + '名称：'+youhuiwuping[i].split('-')[0]+'，数量：'+youhuiwuping[i].split('-')[1]+youhuiwuping[i].split('-')[2]+'\n'
     }
-    console.log('----------------------')
-    console.log('总计：'+ zongjia);
-    console.log('节省：'+ zongyouhui);
-    console.log('**********************');
+//    console.log('----------------------')
+    str = str + '----------------------\n'
+//    console.log('总计：'+ zongjia);
+    str = str + '总计：'+ zongjia.toFixed(2) +'(元)'+'\n'
+ //   console.log('节省：'+ zongyouhui);
+    str = str + '节省：'+ zongyouhui.toFixed(2)+'(元)'+'\n'
+//    console.log('**********************');
+    str = str + '**********************'
 
+    console.log(str);
 }
 //主函数
 function  printInventory(inputs) {
@@ -93,4 +103,3 @@ function  printInventory(inputs) {
     Settle_Account(inputs_new);
 
 }
-
