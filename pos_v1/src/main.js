ac10f996365b5162a5998ad79b47e161931c0010
Likeriver0 <Like_River@163.com>
2014-12-04 17:26:36 +0800

    最终改进版，重构完成

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,39 +1,47 @@
 //TODO: Please write code in this file.
 
+/*函数说明
+*function CountItems(inputs)                        //形成购买商品的结构体数组
+*       ==》function WhetherPromotions(barcode)     //判定是否是优惠产品
+*function Settle_Account(Items_buy)                 //打印总表
+*       ==》function Printfirstpart(Items_buy)      //打印第一部分
+*       ==》function PrintSecondpart(Items_buy)     //打印第一部分
+*       ==》function Printthirdpart(Items_buy)      //打印第一部分
+*function DeleteItems_buy(Items_buy)                //清除数据结构（未用到）
+* */
 
-function GetItemIndex(barcode){
-    var index , i ;
-    for(i = 0 ; i < loadAllItems().length ; i++){
-        if(barcode == loadAllItems()[i].barcode){
-            index = i;
-            break;
-        }
-    }
+//这是一个结构体数组
+var Items_buy = [{
+    barcodes:  undefined,
+    number :    undefined,
+    Promotions : false
+}]
+Items_buy[0] = undefined;
 
-    return index;
-}
+//全局
+var i , str , sum = 0 , Promotions =0;
 
+//获取总表中的地址
+//loadAllItems()[Number(Items_buy[0].barcodes.substring(4,10))];
+
+//存储数量与是否优惠
 function CountItems(inputs){
-    var i , j = 1 , x =0;
-    var inputs_new = new Array();
+    var i;
     for(i = 0 ; i < inputs.length ; i++){
         if(inputs[i].split('-')[1] != undefined){
-            inputs_new[x] = inputs[i];
-            x++
+            Items_buy[Number(inputs[i].split('-')[0].substring(4,10))] = {barcodes : inputs[i].split('-')[0] , number : Number(inputs[i].split('-')[1]) , Promotions:WhetherPromotions(inputs[i].split('-')[0]) }
             continue;
         }
-        if(inputs[i] == inputs[i+1]){
-            j++;
+        if(!(Items_buy[Number(inputs[i].substring(4,10))])){
+            Items_buy[Number(inputs[i].substring(4,10))] = {barcodes : inputs[i] , number : 1 , Promotions :  WhetherPromotions( inputs[i])};
+            continue;
         }
-        else{
-            inputs_new[x] = inputs[i]+'-'+j;
-            x++;
-            j = 1;
-        }
+        Items_buy[Number(inputs[i].substring(4,10))].number++;
     }
-    return inputs_new;
+    return Items_buy
 }
 
+//是否优惠
 function WhetherPromotions(barcode){
     var i;
     for( i = 0 ; i < loadPromotions()[0].barcodes.length ; i++){
@@ -44,62 +52,62 @@ function WhetherPromotions(barcode){
     return false;
 }
 
+//清除数组
+function DeleteItems_buy(Items_buy){
+    for(var i = 0 ; i < Items_buy.length ; i++){
+        Items_buy[i] = undefined;
+    }
+}
 
-function Settle_Account(inputs_new){
-    //获取当前商品号对应的在列表中的位置
-    var i , j = 0, zongyouhui = 0 ,zongjia = 0 ;
-    var youhuiwuping = new Array();
-    var str;
+function Printfirstpart(Items_buy){
+    for(i = 0 ; i < Items_buy.length ; i++){
+        if(!(Items_buy[i]))
+            continue;
+        if((Items_buy[i].Promotions) == true ){ //如果Items_buys是优惠物品
+            str = str + '名称：'+loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].name+'，'+'数量：'+ Items_buy[i].number + loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].unit+'，'+'单价：'+(loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price).toFixed(2)+'(元)，'+'小计：'+((Items_buy[i].number - parseInt(Items_buy[i].number/3)) * loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price).toFixed(2)+'(元)\n';
+            sum = sum + ((Items_buy[i].number - parseInt(Items_buy[i].number/3)) * loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price)
+            continue;
+        }
+        if(Items_buy[i].Promotions == false){//如果Items_buys不是是优惠物品
+            str = str + '名称：'+loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].name+'，'+'数量：'+ Items_buy[i].number + loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].unit+'，'+'单价：'+(loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price).toFixed(2)+'(元)，'+'小计：'+ ((Items_buy[i].number) * loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price).toFixed(2)+'(元)\n';
+            sum = sum + ((Items_buy[i].number) * loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price);
+            continue;
+        }
+    }
+}
 
-//    console.log('***<没钱赚商店>购物清单***');
+function PrintSecondpart(Items_buy){
+    for(i = 0 ; i < Items_buy.length ; i++){
+        if(!(Items_buy[i]))
+            continue;
+        if(Items_buy[i].Promotions){
+            str = str + '名称：'+loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].name+'，数量：'+parseInt(Items_buy[i].number/3)+ loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].unit +'\n'
+            Promotions = Promotions + parseInt(Items_buy[i].number/3)*(loadAllItems()[Number(Items_buy[i].barcodes.substring(4,10))].price)
+        }
+    }
+}
+
+function Printthirdpart(Items_buy){
+    str = str + '总计：'+ sum.toFixed(2) +'(元)'+'\n'
+    str = str + '节省：'+ Promotions.toFixed(2)+'(元)'+'\n'
+}
+
+function Settle_Account(Items_buy){
     str = '***<没钱赚商店>购物清单***\n';
-    for(i = 0 ; i < inputs_new.length ; i++){
-        var index = GetItemIndex(inputs_new[i].split('-')[0]);
-
-        //检测是否属于优惠商品
-        if( WhetherPromotions( inputs_new[i].split('-')[0]) ) {
-            //商品数量除以3就是优惠数量
-//            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
-            str = str + '名称：'+loadAllItems()[index].name+'，'+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+'，'+'单价：'+(loadAllItems()[index].price).toFixed(2)+'(元)，'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)\n';
-            zongyouhui = zongyouhui + parseInt(Number(inputs_new[i].split('-')[1])/3) * loadAllItems()[index].price;
-            zongjia = zongjia + ((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price);
-            youhuiwuping[j] = loadAllItems()[index].name+'-'+parseInt(Number(inputs_new[i].split('-')[1])/3)+'-'+loadAllItems()[index].unit;
-            j++
-        }
-        //不打折时
-        else{
-//            console.log('名称：'+loadAllItems()[index].name+','+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+','+'单价：'+(loadAllItems()[index].price).toFixed(2)+'（元），'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)')
-            str = str + '名称：'+loadAllItems()[index].name+'，'+'数量：'+Number(inputs_new[i].split('-')[1])+loadAllItems()[index].unit+'，'+'单价：'+(loadAllItems()[index].price).toFixed(2)+'(元)，'+'小计：'+((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price).toFixed(2)+'(元)\n';
-            zongjia = zongjia + ((inputs_new[i].split('-')[1] - parseInt(Number(inputs_new[i].split('-')[1])/3)) * loadAllItems()[index].price);
-        }
-
-    }
-
-//    console.log('----------------------')
+    Printfirstpart(Items_buy);
     str = str + '----------------------\n'
-//    console.log('挥泪赠送商品:')
     str = str + '挥泪赠送商品：\n'
-    for(i = 0 ; i < j ; i++ ){
-//        console.log('名称：'+youhuiwuping[i].split('-')[0]+',数量：'+youhuiwuping[i].split('-')[1]+youhuiwuping[i].split('-')[2]);
-        str = str + '名称：'+youhuiwuping[i].split('-')[0]+'，数量：'+youhuiwuping[i].split('-')[1]+youhuiwuping[i].split('-')[2]+'\n'
-    }
-//    console.log('----------------------')
+    PrintSecondpart(Items_buy);
     str = str + '----------------------\n'
-//    console.log('总计：'+ zongjia);
-    str = str + '总计：'+ zongjia.toFixed(2) +'(元)'+'\n'
- //   console.log('节省：'+ zongyouhui);
-    str = str + '节省：'+ zongyouhui.toFixed(2)+'(元)'+'\n'
-//    console.log('**********************');
+    Printthirdpart(Items_buy);
     str = str + '**********************'
-
     console.log(str);
 }
+
 //主函数
 function  printInventory(inputs) {
-
     //计算单个商品个数，形成新的输入表
-    var inputs_new = CountItems(inputs);
+    var Items_buy = CountItems(inputs);
     //结算
-    Settle_Account(inputs_new);
-
+    Settle_Account(Items_buy);
 }
